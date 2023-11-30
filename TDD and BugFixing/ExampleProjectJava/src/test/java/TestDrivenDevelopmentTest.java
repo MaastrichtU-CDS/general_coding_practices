5cbbaccbda6d53db1d64e6f6ad09251838e5d439
florian <florian.vandaalen@maastro.nl>
2023-11-30 13:44:32 +0100

    updated presentation

---
+++ TDD and BugFixing/ExampleProjectJava/src/test/java/TestDrivenDevelopmentTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestDrivenDevelopmentTest {
+    @Test
+    public void sumNegativeIncome() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void sumIncomesCorrect() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void sumMissingIncome() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testRaise() {
+        assertTrue(false);
+    }
+
+    public static void raiseIncome(Person a, double raise) {
+//        ToDo Implement raise income function
+    }
+
+    public class Person {
+
+    }
+
+}
